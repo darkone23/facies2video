3def0c8b148bf6708e65b1c9ba58282c88484400
Thomas Omans <thomas.omans@gmail.com>
2014-04-12 18:14:44 -0700

    taking code out of bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+var head = document.getElementsByTagName("head")[0], script = document.createElement("script"); script.src = "https://rawgit.com/eggsby/facies2video/master/facies2video.js"; head.appendChild(script);
